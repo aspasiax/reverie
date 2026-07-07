e12e341eeb3586c60dfa355105607235e48a8ac8
aspasiax <asxafi@gmail.com>
2026-07-07 18:03:36 +0300

    Initial project setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# Reverie
+
+> Your Movie Journey
+
+A modern full-stack movie journal application built with Spring Boot and React.
---
+++ backend/src/test/java/io/github/aspasiax/reverie/ReverieApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aspasiax.reverie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReverieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
